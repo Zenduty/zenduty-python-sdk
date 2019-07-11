d377d1741c91fc4990c2a568da19b772a90aff51
Vishwa Krishnakumar <vishwa306@gmail.com>
2019-07-11 15:03:22 +0530

    Package name change

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-	name='zenduty',
-	version='1.0',
-	description='SDK to communicate with Zenduty API',
+	name='zenduty-api',
+	version='0.1',
+	description='Python SDK wrapper for the Zenduty API',
+	long_description='Python SDK wrapper for the Zenduty API',
+	long_description_content_type="text/x-rst",
   author='Vishwa Krishnakumar',
 	author_email='vishwa@yellowant.com',
-	url='https://github.com/Zenduty/zenduty-python-sdk',
-  packages=find_packages(exclude='zenduty.*'),
-  install_requires=['urllib3','six'],
+  packages=find_packages(),
+  install_requires=['urllib3','six==1.9.0'],
   scripts=['bin/client.py'])
