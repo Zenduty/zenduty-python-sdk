5678fb8d4142006bb4c3a2411c38164f7e33d480
Mayank Kumar <mayank@ATSs-MacBook-Pro.local>
2023-02-22 13:19:57 +0530

    fix: to_json on converting to string or when printing

--- zenduty/apiV2/serializer.py
+++ zenduty/apiV2/serializer.py
@@ -1,11 +1,14 @@
 import json
 from datetime import datetime
-from types import MappingProxyType
 from uuid import UUID
 
 
 class JsonSerializable(object):
     def to_json(self):
+        return json.dumps(self, default=serialize, sort_keys=True, indent=4)
+    def __repr__(self):
+        return json.dumps(self, default=serialize, sort_keys=True, indent=4)
+    def __str__(self):
         return json.dumps(self, default=serialize, sort_keys=True, indent=4)
 
 
