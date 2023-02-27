88379c0e984d6edab5cb6c9f4a3da5828c89f0ed
Mayank Kumar <mayank@ATSs-MacBook-Pro.local>
2023-02-27 14:56:16 +0530

    chore: all fixes

---
+++ bin/client.py
@@ -0,0 +1,11 @@
+import zenduty
+from zenduty.exceptions import ApiException
+
+api_instance = zenduty.TeamsApi(zenduty.ApiClient("ENTER-YOUR-ACCESS-TOKEN-HERE"))
+
+try:
+    # Get Incidents
+    api_response = api_instance.get_teams()
+    print(api_response.data)
+except ApiException as e:
+    print("Exception when calling TeamsApi->api_account_teams_get: %s\n" % e)
